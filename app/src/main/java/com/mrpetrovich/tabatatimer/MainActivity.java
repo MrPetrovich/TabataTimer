39bdf8de1d1296806e26f16010c4f785b28dde9c
MrPetrovich <ivanchenkostas@gmail.com>
2017-05-15 21:50:00 +0300

    Дизайн таймера

--- app/src/main/java/com/mrpetrovich/tabatatimer/MainActivity.java
+++ app/src/main/java/com/mrpetrovich/tabatatimer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.mrpetrovich.tabatatimer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import res.layout.TimerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    public void onClickStartBtn(View view)
+    {
+        Intent intent = new Intent(this, TimerActivity.class);
+        startActivity(intent);
     }
 }
